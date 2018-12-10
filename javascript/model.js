cf99396ee68e016baeb85e5e976137931b872138
Awais <awais69169@gmail.com>
2018-12-10 20:44:32 +0500

    making model by awais

--- javascript/model.js
+++ javascript/model.js
@@ -35,7 +35,6 @@ function closeModel(){
     model.style.display = "none";
 }
 
-//pending
 function modelImgZoomIn(){
     var currWidth = mdImg.clientWidth;
     if(currWidth > 1000){
@@ -51,4 +50,70 @@ function modelImgZoomOut(){
     }else{
         mdImg.style.width = (currWidth - 100) + "px";
     }
+}
+
+function buy(){
+    var top = document.getElementById("mdTop");
+    var cart = document.getElementById("mdCart");
+    var bottom = document.getElementById("mdBottom");
+    
+    top.style.height = "25%";
+    cart.style.height = "50%";
+    bottom.style.height = "25%";
+    
+    var mdCompany = document.getElementsByClassName("md-company")[0];
+    mdCompany.style.animation = "model-margin 3s";
+    var mdName = document.getElementsByClassName("md-name")[0];
+    mdName.style.animation = "model-margin 3s";
+    var mdPrice = document.getElementsByClassName("md-price")[0];
+    mdPrice.style.animation = "model-margin 3s";
+    var mdDiscount = document.getElementsByClassName("md-discount")[0];
+    mdDiscount.style.display = "none";
+    
+    setTimeout(function(){
+        mdCompany.style.marginTop = "5px";
+        mdCompany.style.marginBottom = "5px";
+        mdName.style.marginTop = "5px";
+        mdName.style.marginBottom = "5px";
+        mdPrice.style.marginTop = "5px";
+        mdPrice.style.marginBottom = "5px";
+    }, 3000);
+    
+    var cartForm = document.getElementById("cartForm");
+    cartForm.style.display = "block";
+    var closeBuy = document.getElementById("closeBuy");
+    closeBuy.style.display = "block";
+}
+
+function closeBuy(){
+    var top = document.getElementById("mdTop");
+    var cart = document.getElementById("mdCart");
+    var bottom = document.getElementById("mdBottom");
+    
+    top.style.height = "49%";
+    cart.style.height = "2%";
+    bottom.style.height = "49%";
+    
+    var mdCompany = document.getElementsByClassName("md-company")[0];
+    mdCompany.style.animation = "model-margin-reverse 3s";
+    var mdName = document.getElementsByClassName("md-name")[0];
+    mdName.style.animation = "model-margin-reverse 3s";
+    var mdPrice = document.getElementsByClassName("md-price")[0];
+    mdPrice.style.animation = "model-margin-reverse 3s";
+    var mdDiscount = document.getElementsByClassName("md-discount")[0];
+    mdDiscount.style.display = "block";
+    
+    setTimeout(function(){
+        mdCompany.style.marginTop = "30px";
+        mdCompany.style.marginBottom = "30px";
+        mdName.style.marginTop = "30px";
+        mdName.style.marginBottom = "30px";
+        mdPrice.style.marginTop = "30px";
+        mdPrice.style.marginBottom = "30px";
+    }, 3000);
+    
+    var cartForm = document.getElementById("cartForm");
+//    cartForm.style.display = "none";
+    var closeBuy = document.getElementById("closeBuy");
+    closeBuy.style.display = "none";
 }
