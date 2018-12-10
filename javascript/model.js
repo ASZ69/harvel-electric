3b8d014d356165810faf77fb3f95ff7984abc611
Awais <awais69169@gmail.com>
2018-12-10 15:26:43 +0500

    making model by awais

--- javascript/model.js
+++ javascript/model.js
@@ -9,11 +9,43 @@ function openModel(id,no){
     var cardImg = card.getElementsByTagName("img")[0];
     var cardPrice = card.getElementsByTagName("h2")[0];
     var cardName = card.getElementsByTagName("p")[0];
-    var cardCompany = card.getElementsByTagName("p")[1];
-    //    alert(cardName.innerText);
+    var cardCompanyDiscount = card.getElementsByTagName("p")[1];
+    var cardDiscount = cardCompanyDiscount.getElementsByTagName("span")[0];
+    var cardCompany = cardCompanyDiscount.innerText.replace(cardDiscount.innerHTML,"");
+    
+    var mdCompany = document.getElementsByClassName("md-company")[0];
+    var mdName = document.getElementsByClassName("md-name")[0];
+    var mdPrice = document.getElementsByClassName("md-price")[0];
+    var mdDiscount = document.getElementsByClassName("md-discount")[0];
+    
+//    alert(cardCompany);
+    mdCompany.textContent = cardCompany;
+    mdName.textContent = cardName.innerText;
+    mdPrice.textContent = cardPrice.innerText;
+    mdDiscount.textContent = cardDiscount.innerText;
 }
 
 function closeModel(){
     var model = document.getElementById("model");
     model.style.display = "none";
+}
+
+//pending
+function modelImgZoomIn(){
+    var modelImg = document.getElementById("modelImg");
+    var currWidth = modelImg.clientWidth;
+    if(currWidth > 1000){
+        return false;
+    }else{
+        modelImg.style.width = (currWidth + 100) + "px";
+    }
+}
+function modelImgZoomOut(){
+    var modelImg = document.getElementById("modelImg");
+    var currWidth = modelImg.clientWidth;
+    if(currWidth < 200){
+        return false;
+    }else{
+        modelImg.style.width = (currWidth - 100) + "px";
+    }
 }
